d1ba054d29c348648451ce1e1f30afa64ce19158
Andrew Kovalenko <goldenduke14@gmail.com>
2023-03-21 15:01:57 +0200

    add 3 exercise

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+const makeGallery = image => {
+  const { preview, original, description } = image;
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(makeGallery).join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+
+player.on(
+  "timeupdate", throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
